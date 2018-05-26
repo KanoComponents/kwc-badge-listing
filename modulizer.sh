fafd272a2e4df48b78d420d320b1140db3fe0f36
Paul Varache <perso@paulvarache.ninja>
2018-05-26 16:12:23 +0100

    Bump version

--- modulizer.sh
+++ modulizer.sh
@@ -1,7 +1,8 @@
 modulizer --out . \
 --npm-name @kano/kwc-badge-listing \
---npm-version 3.0.0-beta.1 \
---dependency-mapping kwc-badge,@kano/kwc-badge,3.0.0-beta.1 \
---dependency-mapping kwc-paper-tooltip,@kano/kwc-paper-tooltip,3.0.0-beta.1 \
---dependency-mapping web-components,@kano/web-components,3.0.0-beta.1 \
---dependency-mapping kwc-style,@kano/kwc-style,3.0.0-beta.1
+--npm-version 3.0.0-beta.2 \
+--import-style name \
+--dependency-mapping kwc-badge,@kano/kwc-badge,3.0.0-beta.2 \
+--dependency-mapping kwc-paper-tooltip,@kano/kwc-paper-tooltip,3.0.0-beta.2 \
+--dependency-mapping web-components,@kano/web-components,3.0.0-beta.2 \
+--dependency-mapping kwc-style,@kano/kwc-style,3.0.0-beta.2
